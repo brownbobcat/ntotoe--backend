9236af09597476ce579df9ef541f39dcab69d289
Kojo-Brown <nicholasbrown988@gmail.com>
2025-05-29 14:54:57 -0400

    fix: add frontend url

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,11 @@ const initializeServer = async () => {
   console.log("Configuring CORS...");
   app.use(
     cors({
-      origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+      origin: [
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "https://ntotoe-frontend-pink.vercel.app",
+      ],
     })
   );
 
